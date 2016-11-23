0adea6beb48e3b5089226ee19596e25c7de453cf
Jason Arnold <jason.arnold.00@gmail.com>
2016-11-23 15:07:55 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,3 +1,31 @@
 export default function createStore(reducer){
 // your code here!
+
+  let state;
+  let listeners = [];
+
+  const getState = function(){
+    return state;
+  }
+
+  const dispatch = function(action){
+    state = reducer(state, action);
+    listeners.forEach(function(listener){
+      return listener();
+    })
+  }
+
+  dispatch({})
+
+  const subscribe = function(listener){
+    listeners.push(listener);
+  }
+    
+
+  return {
+    getState: getState,
+    dispatch: dispatch,
+    subscribe: subscribe
+  }
+
 }
